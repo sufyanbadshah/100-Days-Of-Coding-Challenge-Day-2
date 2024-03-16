2ddd2dcf10ae6ab16d745cfae0e8055236e0c0a1
Sufyan Badshah <sufyanbadshah898@gmail.com>
2024-03-16 23:56:28 +0500

    Add files via upload

---
+++ Day2/day2.ts
@@ -0,0 +1,11 @@
+ console.log (`Khabib Once Said,"I don't train to fight I train to Dominate"`);
+ 
+ let famous_person = "Khabib"
+ let message = `${famous_person} once said,"I don't train to fight I train to Dominate"`;
+ console.log(message);
+
+ let Name = "\t\n Sufyan\t\n"
+ console.log(`Hello ${Name}`)
+ 
+ let Name = "\t\n Sufyan\t\n"
+ console.log (`Hello ${Name.trim()}`)
