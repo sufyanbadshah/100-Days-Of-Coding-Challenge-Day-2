8964bd5485029e48d435085686ae0a205ea762b9
Sufyan Badshah <sufyanbadshah898@gmail.com>
2024-03-16 23:53:27 +0500

    Delete Day2 directory

--- Day2/day2.ts
+++
@@ -1,8 +0,0 @@
- console.log (`Khabib Once Said,"I don't train to fight I train to Dominate"`);
- 
- let famous_person = "Khabib"
- let message = `${famous_person} once said,"I don't train to fight I train to Dominate"`;
- console.log(message);
-
- let Name = "\t\n Sufyan\t\n"
- console.log (`Hello ${Name.trim()}`)
